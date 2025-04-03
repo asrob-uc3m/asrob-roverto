78b77815a4e1023445e0dcc19604333bf2653d5b
anacg1620 <anacg1620@gmail.com>
2025-04-03 20:25:34 +0200

    lidar pid launch

---
+++ perception/launch/lidar_pid_launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('ydlidar_ros2_driver'),
+                    'launch',
+                    'ydlidar_launch.py'
+                ])
+            ]),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('osr_bringup'),
+                    'launch',
+                    'osr_launch.py'
+                ])
+            ]),
+        ),
+        Node(
+            package='perception',
+            namespace='perception',
+            executable='pid',
+            name='lidar_pid'
+        )
+    ])
