7913da81caacd5b3614d9dace44ff20452162fe6
anacg1620 <anacg1620@gmail.com>
2025-04-26 14:01:10 +0200

    launcher prueba4

---
+++ perception/launch/prueba4_launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('orbbec_camera'),
+                    'launch',
+                    'astra_pro_plus.launch.py'
+                ])
+            ]),
+        ),
+        Node(
+            package='perception',
+            namespace='perception',
+            executable='number',
+            name='number_detection'
+        ),
+        Node(
+            package='perception',
+            namespace='perception',
+            executable='box_color',
+            name='box_color_detection'
+        ),
+        Node(
+            package='perception',
+            namespace='perception',
+            executable='aruco',
+            name='aruco_detection'
+        ),
+        Node(
+            package='perception',
+            namespace='perception',
+            executable='spin',
+            name='prueba4',
+            output='prueba4'
+        )
+    ])
